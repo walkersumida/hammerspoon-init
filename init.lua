d956d730b12533e1e4843629f6eeba45e4d32c6d
WalkerSumida <walkersumida@gmail.com>
2017-01-17 21:10:05 +0900

    Init

---
+++ init.lua
@@ -0,0 +1,92 @@
+-- kana <--> eisuu binds
+local prevKeyCode
+local leftCommand = 0x37
+local rightCommand = 0x36
+local eisuu = 0x66
+local kana = 0x68
+
+local function keyStroke(modifiers, character)
+   hs.eventtap.keyStroke(modifiers, character)
+end
+
+local function jp()
+   keyStroke({}, kana)
+end
+
+local function eng()
+   keyStroke({}, eisuu)
+end
+
+local function handleEvent(e)
+   local keyCode = e:getKeyCode()
+
+   local isCmdKeyUp = not(e:getFlags()['cmd']) and e:getType() == hs.eventtap.event.types.flagsChanged
+   if isCmdKeyUp and prevKeyCode == leftCommand then
+      eng()
+   elseif isCmdKeyUp and prevKeyCode == rightCommand then
+      jp()
+   end
+
+   prevKeyCode = keyCode
+end
+
+eventtap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged, hs.eventtap.event.types.keyDown, hs.eventtap.event.types.keyUp}, handleEvent)
+eventtap:start()
+
+
+-- other binds
+local function keyCode(key, modifiers)
+   modifiers = modifiers or {}
+   return function()
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+      hs.timer.usleep(1000)
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
+   end
+end
+
+local function remapKey(modifiers, key, keyCode)
+   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
+
+local function disableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:disable()
+   end
+end
+
+local function enableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:enable()
+   end
+end
+
+local function handleGlobalAppEvent(name, event, app)
+   if event == hs.application.watcher.activated then
+      -- hs.alert.show(name)
+      if name ~= "iTerm2" and name ~= "ターミナル" and name ~= "Emacs" then
+         enableAllHotkeys()
+      else
+         disableAllHotkeys()
+      end
+   end
+end
+
+appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
+appsWatcher:start()
+
+remapKey({'ctrl'}, 'f', keyCode('right'))
+remapKey({'ctrl'}, 'b', keyCode('left'))
+remapKey({'ctrl'}, 'n', keyCode('down'))
+remapKey({'ctrl'}, 'p', keyCode('up'))
+
+remapKey({'ctrl'}, 'w', keyCode('x', {'cmd'}))
+remapKey({'ctrl'}, 'y', keyCode('v', {'cmd'}))
+
+remapKey({'ctrl'}, 's', keyCode('f', {'cmd'}))
+remapKey({'ctrl'}, '/', keyCode('z', {'cmd'}))
+remapKey({'ctrl'}, 'g', keyCode('escape'))
+
+remapKey({'ctrl'}, 'v', keyCode('pagedown'))
+remapKey({'alt'}, 'v', keyCode('pageup'))
+remapKey({'cmd', 'shift'}, ',', keyCode('home'))
+remapKey({'cmd', 'shift'}, '.', keyCode('end'))
