a421e42b78ddb4e629d6f4591e7aaa8df338db76
WalkerSumida <walkersumida@gmail.com>
2019-12-14 20:52:14 +0900

    feat: add keybindings for vscode

--- init.lua
+++ init.lua
@@ -48,6 +48,33 @@ local function remapKey(modifiers, key, keyCode)
    hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
 end
 
+local function hotkeys(application)
+  if application ~= 'vscode' then
+    remapKey({'ctrl'}, 'f', keyCode('right'))
+    remapKey({'ctrl'}, 'b', keyCode('left'))
+    remapKey({'ctrl'}, 'n', keyCode('down'))
+    remapKey({'ctrl'}, 'p', keyCode('up'))
+    remapKey({'ctrl'}, 'h', keyCode('delete'))
+  end
+  remapKey({'ctrl'}, 'e', keyCode('right', {'cmd'}))
+  remapKey({'ctrl'}, 'a', keyCode('left', {'cmd'}))
+
+  remapKey({'ctrl'}, 'w', keyCode('x', {'cmd'}))
+  remapKey({'ctrl'}, 'y', keyCode('v', {'cmd'}))
+
+  remapKey({'ctrl'}, 'i', keyCode('tab'))
+  remapKey({'ctrl'}, 'm', keyCode('return'))
+  remapKey({'ctrl'}, 's', keyCode('f', {'cmd'}))
+  remapKey({'ctrl'}, '/', keyCode('z', {'cmd'}))
+  remapKey({'ctrl'}, '-', keyCode('z', {'cmd'}))
+  remapKey({'ctrl'}, '[', keyCode('escape'))
+
+  remapKey({'ctrl'}, 'v', keyCode('pagedown'))
+  remapKey({'alt'}, 'v', keyCode('pageup'))
+  remapKey({'cmd', 'shift'}, ',', keyCode('home'))
+  remapKey({'cmd', 'shift'}, '.', keyCode('end'))
+end
+
 local function disableAllHotkeys()
    for k, v in pairs(hs.hotkey.getHotkeys()) do
       v['_hk']:disable()
@@ -55,44 +82,25 @@ local function disableAllHotkeys()
 end
 
 local function enableAllHotkeys()
-   for k, v in pairs(hs.hotkey.getHotkeys()) do
-      v['_hk']:enable()
-   end
+   hotkeys('general')
+end
+
+local function enableHotkeysForVSCode()
+   hotkeys('vscode')
 end
 
 local function handleGlobalAppEvent(name, event, app)
    if event == hs.application.watcher.activated then
       -- hs.alert.show(name)
-      if name ~= "iTerm2" and name ~= "ターミナル" and name ~= "Emacs" then
-         enableAllHotkeys()
-      else
+      if name == "iTerm2" or name == "ターミナル" or name == "Emacs" then
          disableAllHotkeys()
+      elseif name == "Code" then
+         enableHotkeysForVSCode()
+      else
+         enableAllHotkeys()
       end
    end
 end
 
 appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
 appsWatcher:start()
-
-remapKey({'ctrl'}, 'f', keyCode('right'))
-remapKey({'ctrl'}, 'b', keyCode('left'))
-remapKey({'ctrl'}, 'n', keyCode('down'))
-remapKey({'ctrl'}, 'p', keyCode('up'))
-remapKey({'ctrl'}, 'e', keyCode('right', {'cmd'}))
-remapKey({'ctrl'}, 'a', keyCode('left', {'cmd'}))
-
-remapKey({'ctrl'}, 'w', keyCode('x', {'cmd'}))
-remapKey({'ctrl'}, 'y', keyCode('v', {'cmd'}))
-
-remapKey({'ctrl'}, 'i', keyCode('tab'))
-remapKey({'ctrl'}, 'm', keyCode('return'))
-remapKey({'ctrl'}, 's', keyCode('f', {'cmd'}))
-remapKey({'ctrl'}, '/', keyCode('z', {'cmd'}))
-remapKey({'ctrl'}, '-', keyCode('z', {'cmd'}))
-remapKey({'ctrl'}, 'h', keyCode('delete'))
-remapKey({'ctrl'}, '[', keyCode('escape'))
-
-remapKey({'ctrl'}, 'v', keyCode('pagedown'))
-remapKey({'alt'}, 'v', keyCode('pageup'))
-remapKey({'cmd', 'shift'}, ',', keyCode('home'))
-remapKey({'cmd', 'shift'}, '.', keyCode('end'))
